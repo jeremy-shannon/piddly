bea666ea6a8d618d0c4c55564b6a6b2d01f7ac89
Jeremy Shannon <jeremyeshannon@gmail.com>
2015-02-16 13:12:55 -0600

    angular and random button and name validation yadda yadda

---
+++ core.js
@@ -0,0 +1,60 @@
+// public/core.js
+
+var app = angular.module('jeremyApp', []);
+
+app.controller('MainCtrl', function($scope) {
+    $scope.theNumber = 0;
+
+    $scope.getNumber = function() {
+        console.log(Math.random());
+        $scope.theNumber = 10 * Math.random();
+    };
+});
+
+
+/*
+angular.module('jeremyApp', [])
+    .controller('mainController',['$scope', function($scope) {
+        $scope.theNumber = '0';
+       $scope.getNumber = function() {
+            $scope.theNumber = 10 * Math.random();
+        };
+    }]);
+
+    // when landing on the page, get all todos and show them
+    $http.get('/api/todos')
+        .success(function(data) {
+            $scope.todos = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    // when submitting the add form, send the text to the node API
+    $scope.createTodo = function() {
+        $http.post('/api/todos', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // delete a todo after checking it
+    $scope.deleteTodo = function(id) {
+        $http.delete('/api/todos/' + id)
+            .success(function(data) {
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+    */
+
+
--- index.js
+++ index.js
@@ -1,16 +1,63 @@
 var express = require('express');
 var cool = require('cool-ascii-faces');
 var app = express();
+var mongodb = require('mongodb');
+var mongoose = require('mongoose');
+var path = require('path');
 
+
+// Here we find an appropriate database to connect to, defaulting to
+// localhost if we don't find one.
+console.log(process.env.MONGOLAB_URI);
+var uristring =
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb://localhost/HelloMongoose';
+
+// The http server will listen to an appropriate port, or default to
+// port 5000.
+var theport = process.env.PORT || 5000;
+
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+      console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+      console.log ('Succeeded connected to: ' + uristring);
+  }
+});
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  var result = ''
-  var times = process.env.TIMES || 5
-  for (i=0; i < times; i++)
-    result += cool();
-  response.send(result);
+
+var mySchema = new mongoose.Schema({
+  myname: String
+});
+
+var myNames = mongoose.model('Test',mySchema, 'test');
+
+app.get('/', function(req, res) {
+    var theName = '';
+    myNames.findOne({}).exec(function(err,result) {
+        if (!err) {
+ /*           console.log('hey');
+            console.log(result);
+            theName = result.myname;
+            console.log(theName);
+            var outputString = '';
+            var times = process.env.TIMES * 2 || 5
+            for (i=0; i < times; i++) {
+                outputString += theName + '<br>';
+                outputString += cool() + '<br>';
+            }*/
+            res.sendFile(path.join(__dirname + '/index.html'));
+        }
+    });
+});
+
+app.get('/core.js', function(req, res) {
+    res.sendFile(path.join(__dirname + '/core.js'));
 });
 
 app.listen(app.get('port'), function() {
