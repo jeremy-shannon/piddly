9b4ac85627c772c4f38930193e8fa0e498251aea
Jeremy Shannon <jeremyeshannon@gmail.com>
2015-02-28 16:42:50 -0600

    made the randomization of the number a little more fun with some seriously brain-numbing timer functions

--- core.js
+++ core.js
@@ -2,21 +2,63 @@
 
 var app = angular.module('jeremyApp', []);
 
-app.controller('MainCtrl', function($scope, $http) {
+app.controller('MainCtrl', function($scope, $http, $interval, $timeout) {
     $scope.theNumber = 0;
     $scope.rowClass = '';
-    $scope.listLength = 10;
+    $scope.listLength = 20;
     var alertState = false;
     $scope.currentScore = 0;
     $scope.currentRank = 0;
     $scope.currenName = '';
+    var numberOfDigits = 8;
 
     $scope.showAlert = function() {
         return alertState;
     };
 
     $scope.getNumber = function() {
-        $scope.theNumber = 10 * Math.random();
+        var storedNumber, digit;
+        storedNumber = 0;
+        digit = 0;
+
+        // for(j=0;j<99;j++) {
+        //     $timeout(function(){
+        //         digit = Math.floor(10*Math.random());
+        //         digit = (digit * Math.pow(10,(-2))).toFixed(2);
+        //         $scope.theNumber = digit ;
+        //     },j);
+        // }
+
+        // $timeout(function(){
+        //     storedNumber = digit*1;
+        //     console.log(storedNumber);
+        // },100);
+
+        // for(j=0;j<99;j++) {
+        //     $timeout(function(){
+        //         digit = Math.floor(10*Math.random());
+        //         digit = (digit * Math.pow(10,(-1)));
+        //         digit = (digit*1 + storedNumber).toFixed(2);
+        //         $scope.theNumber = digit;
+        //     },j+100);
+        // }
+
+
+        for(i=0;i<numberOfDigits;i++) {
+            (function(index){
+                $timeout(function(){
+                    $interval(function(){
+                        console.log(index);
+                        digit = Math.floor(10*Math.random());
+                        digit = (digit * Math.pow(10,(-(numberOfDigits-1) + index))).toFixed(numberOfDigits-1);
+                        digit = (1*digit + 1*storedNumber).toFixed(numberOfDigits-1);
+                        $scope.theNumber = digit;
+                    }, 1, 33);
+                    storedNumber = digit;
+                },160*index);
+            })(i);
+        }
+
         alertState = false;
     };
 
